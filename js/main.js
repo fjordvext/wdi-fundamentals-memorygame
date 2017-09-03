3f4607654cc4afdc9ac40aa3ce1f3c425f85f387
Robert K Sosin <robert@sosincerely.com>
2017-09-03 11:27:48 -0400

    created game logic r4

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped queen");
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("user flipped king");
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("you found a match!");
+var checkForMatch = function() {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-			alert("sorry, try again");
+		console.log("Sorry, try again.");
 		}
+	}
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
 	};
+	flipCard(0);
+	flipCard(2);
