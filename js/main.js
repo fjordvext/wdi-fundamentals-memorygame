7f86c1772a76054fe70f2730920b6d492a4f002c
Robert K Sosin <robert@sosincerely.com>
2017-09-05 17:22:50 -0400

    created game logic r5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "Queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "King",
+	suit: "Hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,8 +29,10 @@ var checkForMatch = function() {
 		}
 	}
 var flipCard = function(cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit)
 	checkForMatch();
 	};
 	flipCard(0);
