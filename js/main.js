c4da72a7543d5381ca82554c64c9b0feffebbb15
Robert K Sosin <robert@sosincerely.com>
2017-09-14 14:00:43 -0400

    tarot version shuffling

--- js/main.js
+++ js/main.js
@@ -1,34 +1,158 @@
-var cards = [
+// Here's the deck of 22 Major Arcana cards. Images from the Jodorowsky set of Marseilles Tarot.
+
+var deck = [
 {
-	rank: "Queen",
-	suit: "Hearts",
-	cardImage: "images/queen-of-hearts.png"
+	rank: "The Fool",
+	suit: "Arcana",
+	cardImage: "images/fool.png"
 },
 {
-	rank: "Queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	rank: "The Magician",
+	suit: "Arcana",
+	cardImage: "images/magician.png"
 },
 {
-	rank: "King",
-	suit: "Hearts",
-	cardImage: "images/king-of-hearts.png"
+	rank: "The Prophetess",
+	suit: "Arcana",
+	cardImage: "images/prophetess.png"
 },
 {
-	rank: "King",
-	suit: "Diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	rank: "The Empress",
+	suit: "Arcana",
+	cardImage: "images/empress.png"
+},
+{
+	rank: "The Emperor",
+	suit: "Arcana",
+	cardImage: "images/emperor.png"
+},
+{
+	rank: "The Lovers",
+	suit: "Arcana",
+	cardImage: "images/lovers.png"
+},
+{
+	rank: "The Chariot",
+	suit: "Arcana",
+	cardImage: "images/chariot.png"
+},
+{
+	rank: "Justice",
+	suit: "Arcana",
+	cardImage: "images/justice.png"
+},
+{
+	rank: "The Hermit",
+	suit: "Arcana",
+	cardImage: "images/hermit.png"
+},
+{
+	rank: "Wheel of Fortune",
+	suit: "Arcana",
+	cardImage: "images/wheel-of-fortune.png"
+},
+{
+	rank: "Strength",
+	suit: "Arcana",
+	cardImage: "images/strength.png"
+},
+{
+	rank: "The Hanged Man",
+	suit: "Arcana",
+	cardImage: "images/hanged-man.png"
+},
+{
+	rank: "Death",
+	suit: "Arcana",
+	cardImage: "images/death.png"
+},
+{
+	rank: "Temperance",
+	suit: "Arcana",
+	cardImage: "images/temperance.png"
+},
+{
+	rank: "The Devil",
+	suit: "Arcana",
+	cardImage: "images/devil.png"
+},
+{
+	rank: "The Tower",
+	suit: "Arcana",
+	cardImage: "images/tower.png"
+},
+{
+	rank: "The Star",
+	suit: "Arcana",
+	cardImage: "images/star.png"
+},
+{
+	rank: "The Moon",
+	suit: "Arcana",
+	cardImage: "images/moon.png"
+},
+{
+	rank: "The Sun",
+	suit: "Arcana",
+	cardImage: "images/sun.png"
+},
+{
+	rank: "Judgment",
+	suit: "Arcana",
+	cardImage: "images/judgment.png"
+},
+{
+	rank: "The World",
+	suit: "Arcana",
+	cardImage: "images/world.png"
 }
 ];
+
+
+// Shuffling the array deck using the Fisher Yates method (found at https://bost.ocks.org/mike/shuffle/).
+function shuffle(deck) {
+  var m = deck.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = deck[m];
+    deck[m] = deck[i];
+    deck[i] = t;
+  }
+
+  return deck;
+}
+
+// Making the cards array the first seven cards of the newly shuffled deck.
+// To get the first n elements of an array, use this code (from https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array)
+var cardsSingle = deck.slice(0, 7);
+
+//Then taking that array and doubling it using concat (found at https://www.w3schools.com/jsref/jsref_concat_array.asp)
+var cardsDouble = cardsSingle;
+var cards = cardsSingle.concat(cardsDouble);
+
+
+// Shuffling the array cards using the Fisher Yates method.
+shuffle(cards);
+
+// Checking two flipped cards to see if they match.
+
 var cardsInPlay = [];
 var checkForMatch = function() {
-	if (cardsInPlay.length === 2)
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+	if (cardsInPlay.length % 2 === 0)
+		if (cardsInPlay[cardsInPlay.length-1] === cardsInPlay[cardsInPlay.length -2]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 		}
 	}
+
+// Flipping cards.
 var flipCard = function() {
 	var cardId = this.getAttribute('data-id')
 	this.setAttribute('src', cards[cardId].cardImage);
@@ -38,6 +162,8 @@ var flipCard = function() {
 	console.log(cards[cardId].suit)
 	checkForMatch();
 	};
+
+// Creating board.
 var createBoard = function(){
 for (var i = 0; i < cards.length; i++) {
 	var cardElement = document.createElement('img');
